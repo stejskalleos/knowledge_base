f36f51ada2b5e7beab59f50ad6ca0c4bd63c42ff
Leos Stejskal <github@stejskalleos.cz>
2026-07-28 10:42:50 +0200

    Add -y flag to dnf upgrade and install commands

--- foreman/scripts/foremactl_nightly.sh
+++ foreman/scripts/foremactl_nightly.sh
@@ -2,5 +2,5 @@
 dnf clean all
 dnf install -y https://yum.theforeman.org/releases/nightly/el9/x86_64/foreman-release.rpm
 dnf install -y https://yum.theforeman.org/katello/nightly/katello/el9/x86_64/katello-repos-latest.rpm
-dnf upgrade
-dnf install foremanctl
+dnf upgrade -y
+dnf install -y foremanctl
